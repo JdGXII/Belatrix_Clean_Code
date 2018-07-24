90b56ba56016508bd0c6faaea2a89de5399e1a69
JdGXII <jralzaibar@yahoo.com>
2018-07-24 17:09:45 -0500

    Correcion de ejercicio Interface Segregation

--- SOLID/04 Interface Segregation/IPrintTasks.cs
+++ SOLID/04 Interface Segregation/IPrintTasks.cs
@@ -2,15 +2,6 @@
 
 namespace SOLID._04_Interface_Segregation
 {
-    public interface IPrintTasks
-    {
-        bool PrintContent(string content);
-        bool ScanContent(string content);
-        bool FaxContent(string content);
-        bool PhotoCopyContent(string content);
-        bool PrintDuplexContent(string content);
-    }
-
     public interface IBasicPrintable
     {
         bool PrintContent(string content);
